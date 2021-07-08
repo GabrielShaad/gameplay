37b5c16d06c14c66b5c58f9415cbff1beacaf686
Gabriel Alexandre <gabriel.shaad@gmail.com>
2021-07-07 23:25:17 -0300

    Adiciona discord oauth e api de user

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, LogBox } from "react-native";
+import AppLoading from "expo-app-loading";
 import { useFonts } from "expo-font";
 import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
 import {
   Rajdhani_500Medium,
   Rajdhani_700Bold,
 } from "@expo-google-fonts/rajdhani";
-import AppLoading from "expo-app-loading";
+
+import { AuthProvider } from "./src/hooks/auth";
 
 import { Routes } from "./src/routes";
 import { Background } from "./src/components/Background";
+
+LogBox.ignoreLogs([
+  "You are not currently signed in to Expo on your development machine",
+]);
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -30,7 +36,9 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </Background>
   );
 }
